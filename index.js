1bc657ef6895e8cdf2669b25796053b1e376a76c
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-05-03 04:15:53 +0300

    added changes the js page

---
+++ index.js
@@ -0,0 +1,42 @@
+var p1Monday = 510
+var p2Monday =308
+var  p3Monday =486
+var p4Monday= 572
+
+var p1Tuesday =510
+var p2Tuesday =308
+var p3Tuesday =486
+var p3Tuesday =572
+
+var p1Wednesday = 510
+var p2Wednesday =308
+var p3Wednesday = 486
+var p4Wednesday = 572
+
+var p1Thursday = 510
+var p2Thursday = 308
+var p3Thurday = 486
+var p4Thursday =572
+
+var p1Friday = 510
+var p2Friday = 308
+var p3Friday = 486
+var p4Friday = 572
+
+var p1Saturday = 510
+var p2Saturday = 308
+var p3Saturday = 486
+var p4Saturday = 572
+
+var p1Sunday = 510
+var p2Sunday = 308
+var p3Sunday = 486
+var p4Sunday = 486
+
+var shadeA = function(){
+    alert("Your production in Shed A  is " + p1Monday + "litres per day")
+    
+}
+var shadeB =
+
+
